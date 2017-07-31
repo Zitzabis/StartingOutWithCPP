64f63060448642740051d0886032d4bb18572d48
Stephen Floyd <zitzabis.zitz@gmail.com>
2017-07-31 20:45:56 +1000

    Ch2Ex1: Adds file in completed form.

---
+++ StartingOutWithCPP/Programming Challenges/Ch2/Ex1/Ex1.cpp
@@ -0,0 +1,21 @@
+// Include
+#include <iostream>
+
+/*
+	It is poor practice to use the "standard" namespace.
+	However it is included so as to adhere to what the book does.
+*/
+using namespace std;
+
+// Main
+int main() {
+	// Vars
+	int a = 50;
+	int b = 100;
+
+	// Calculate total
+	int total = a + b;
+
+	// End
+	return 0;
+}
