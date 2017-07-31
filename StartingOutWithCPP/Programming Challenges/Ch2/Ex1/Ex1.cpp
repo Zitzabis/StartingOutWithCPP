4e5be7040850a618f406507b21cc0a7c764e7087
Stephen Floyd <zitzabis.zitz@gmail.com>
2017-07-31 20:49:57 +1000

    Ch2Ex1: Adds documentation.

--- StartingOutWithCPP/Programming Challenges/Ch2/Ex1/Ex1.cpp
+++ StartingOutWithCPP/Programming Challenges/Ch2/Ex1/Ex1.cpp
@@ -1,3 +1,17 @@
+/*
+	== Documentation ==
+
+	Project:	Ch2Ex1
+	Author:		S. Floyd
+	Date:		07/31/2017
+
+	== Sum of Two Numbers ==
+	Write a program that stores the integers 50 and 100 in variables, and stores the sum of
+	these two in a variable named total.
+*/
+
+
+
 // Include
 #include <iostream>
 
