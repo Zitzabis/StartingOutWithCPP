60ce89acda60be5acba8242adcdf9222fe4ce625
Stephen Floyd <zitzabis.zitz@gmail.com>
2017-08-01 15:23:00 +1000

    Ch2Ex8: Adds more documentation.

--- StartingOutWithCPP/Ch2Ex8/Ex8.cpp
+++ StartingOutWithCPP/Ch2Ex8/Ex8.cpp
@@ -37,9 +37,11 @@ int main() {
 	double item5 = 3.95;
 	double salesTax = 0.07;
 
+	// Calculate
 	double subtotal = item1 + item2 + item3 + item4 + item5;
 	double total = subtotal + (subtotal * salesTax);
 
+	// Print results
 	cout << "Item 1			" << item1 << endl;
 	cout << "Item 2			" << item2 << endl;
 	cout << "Item 3			" << item3 << endl;
