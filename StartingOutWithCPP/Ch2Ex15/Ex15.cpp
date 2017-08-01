3638be8f45da0cc223989867e41a76e8f650bf70
Stephen Floyd <zitzabis.zitz@gmail.com>
2017-08-01 17:26:21 +1000

    Ch2Ex15: Adds project.

---
+++ StartingOutWithCPP/Ch2Ex15/Ex15.cpp
@@ -0,0 +1,31 @@
+/*
+	== Documentation ==
+	Project:	Ch2Ex15
+	Author:		S. Floyd
+	Date:		08/01/2017
+
+	== Triangle Pattern ==
+	Write a program that displays the following pattern on the screen:
+	   *
+	  ***
+	 *****
+	******* 
+*/
+
+// Include
+#include <iostream>
+
+/*
+	It is poor practice to use the "standard" namespace.
+	However it is included so as to adhere to what the book does.
+*/
+using namespace std;
+
+// Main
+int main() {
+	// Print result
+	cout << "   *\n" << "  ***\n" << " *****\n" << "*******\n" << endl;
+
+	// End
+	return 0;
+}
