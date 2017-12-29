61e8fd7566330d014422887f4363fdf63f32817b
Stephen Floyd <zitzabis.zitz@gmail.com>
2017-12-29 13:10:56 -0500

    Ch3Ex10: Adds project.

---
+++ StartingOutWithCPP/Ch3Ex10/Ch3Ex10.cpp
@@ -0,0 +1,39 @@
+/*
+	== Documentation ==
+	Project:	Ch3Ex10
+	Author:		S. Floyd
+	Date:		12/29/2017
+
+	== How Much Insurance? ==
+	Many financial experts advise that property owners should insure their homes
+	or buildings for at least 80 percent of the amount it would cost to replace the
+	structure. Write a program that asks the user to enter the replacement cost of a
+	building and then displays the minimum amount of insurance he or she should buy
+	for the property.
+*/
+
+// Include
+#include <iostream>
+#include <string>
+
+/*
+	It is poor practice to use the "standard" namespace.
+	However it is included so as to adhere to what the book does.
+*/
+using namespace std;
+
+// Main
+int main() {
+	// Vars
+	double buildingValue;
+
+	// Input
+	cout << "Replacement cost of a building: ";
+	cin >> buildingValue;
+
+	// Print results
+	cout << endl << "Insurance to be bought for the building: " << buildingValue * 0.8 << endl;
+
+	// End	
+	return 0;
+}
